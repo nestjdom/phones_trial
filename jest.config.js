279682ebd92a1549a4661a546a3a4d2a72f4a74f
Néstor Juan Domínguez Diaz <nestorjdd@ext.inditex.com>
2025-06-13 13:45:56 +0100

    chore: :wrench: Add Jest configuration and testing setup

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
+
+module.exports = createJestConfig(customJestConfig) 
