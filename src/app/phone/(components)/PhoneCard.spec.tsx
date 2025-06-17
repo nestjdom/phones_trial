a250f7c78970b2b231fc83c82165f7ff1d984477
Néstor Juan Domínguez Diaz <nestorjdd@ext.inditex.com>
2025-06-17 12:25:34 +0100

    test: :white_check_mark: Update PhoneCard tests to use role assertions

--- src/app/phone/(components)/PhoneCard.spec.tsx
+++ src/app/phone/(components)/PhoneCard.spec.tsx
@@ -6,7 +6,7 @@ const mockPhone = mockPhones[0];
 
 test('should render link, image, name and price', async ({ mount }) => {
   const component = await mount(<PhoneCard phone={mockPhone} />);
-  // await expect(component.getByRole('link')).toBeVisible();
+  await expect(component).toHaveRole('link');
   await expect(component.getByRole('img')).toBeVisible();
   await expect(component.getByRole('heading', { name: mockPhone.name })).toBeVisible();
   await expect(component.getByText(new RegExp(mockPhone.basePrice.toString()))).toBeVisible();
