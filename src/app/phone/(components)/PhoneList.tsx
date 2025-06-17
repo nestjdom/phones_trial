c43fac407fbe64a7f66a93b533eedaf122ade512
Néstor Juan Domínguez Diaz <nestorjdd@ext.inditex.com>
2025-06-17 16:01:32 +0100

    refactor: :art: Update PhoneList component layout to improve responsiveness with new grid column settings

--- src/app/phone/(components)/PhoneList.tsx
+++ src/app/phone/(components)/PhoneList.tsx
@@ -27,7 +27,7 @@ export default async function PhoneList({ search }: PhoneListProps) {
           <>
             <div className='text--sm text--gray-600'>{phones.length} {texts.results}</div>
 
-            <ul className='grid grid--cols-2 grid--sm-cols-3 grid--lg-cols-4 grid--xl-cols-5 grid-with-borders'>
+            <ul className='grid grid--cols-1 grid--sm-cols-2 grid--lg-cols-4 grid--xl-cols-5 grid-with-borders'>
               {phones.map((phone, index) => (
                 <li key={phone.id + index}>
                   <PhoneCard phone={phone} />
